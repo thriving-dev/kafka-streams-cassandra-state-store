8396eaf55fc61a480f3679239bb6fa92dca6fc77
hartmut <10864443+hartmut-co-uk@users.noreply.github.com>
2023-03-07 22:14:43 +0100

    feat: adds first integration test 'WordCount' with testcontainers

---
+++ kafka-streams-cassandra-state-store/src/intTest/java/dev/thriving/oss/kafka/streams/cassandra/state/store/AbstractIntegrationTest.java
@@ -0,0 +1,95 @@
+package dev.thriving.oss.kafka.streams.cassandra.state.store;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.google.common.collect.ImmutableMap;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+import org.jetbrains.annotations.NotNull;
+import org.testcontainers.containers.CassandraContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.redpanda.RedpandaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.Properties;
+import java.util.UUID;
+
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    protected static final String CASSANDRA_KEYSPACE = "test";
+
+    @Container
+    public RedpandaContainer redpanda = new RedpandaContainer(
+            DockerImageName.parse("docker.redpanda.com/vectorized/redpanda:v23.1.1")
+    );
+
+    @Container
+    public CassandraContainer<?> cassandra = new CassandraContainer<>(
+            DockerImageName.parse("cassandra:4.1")
+    ).withInitScript("schema.cql");
+
+    @NotNull
+    protected Properties getStreamsProperties() {
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "integration-test");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, redpanda.getBootstrapServers());
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        return props;
+    }
+
+    @NotNull
+    protected AdminClient initAdminClient() {
+        return AdminClient.create(
+                ImmutableMap.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, redpanda.getBootstrapServers())
+        );
+    }
+
+    @NotNull
+    protected <K, V> KafkaProducer<K, V> initProducer(Serde<K> keySerde, Serde<V> valueSerde) {
+        return new KafkaProducer<>(
+                ImmutableMap.of(
+                        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                        redpanda.getBootstrapServers(),
+                        ProducerConfig.CLIENT_ID_CONFIG,
+                        UUID.randomUUID().toString()
+                ),
+                keySerde.serializer(),
+                valueSerde.serializer()
+        );
+    }
+
+    @NotNull
+    protected <K, V> KafkaConsumer<K, V> initConsumer(Serde<K> keySerde, Serde<V> valueSerde) {
+        return new KafkaConsumer<>(
+                ImmutableMap.of(
+                        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                        redpanda.getBootstrapServers(),
+                        ConsumerConfig.GROUP_ID_CONFIG,
+                        "tc-" + UUID.randomUUID(),
+                        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                        "earliest"
+                ),
+                keySerde.deserializer(),
+                valueSerde.deserializer()
+        );
+    }
+
+    @NotNull
+    protected CqlSession initSession() {
+        return CqlSession.builder()
+                .withKeyspace(CASSANDRA_KEYSPACE)
+                .withLocalDatacenter(cassandra.getLocalDatacenter())
+                .addContactPoint(cassandra.getContactPoint())
+                .build();
+    }
+
+}
---
+++ kafka-streams-cassandra-state-store/src/intTest/java/dev/thriving/oss/kafka/streams/cassandra/state/store/WordCountCassandraStoreTest.java
@@ -0,0 +1,150 @@
+package dev.thriving.oss.kafka.streams.cassandra.state.store;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.rnorth.ducttape.unreliables.Unreliables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.apache.kafka.common.utils.Utils.mkEntry;
+import static org.apache.kafka.common.utils.Utils.mkMap;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WordCountCassandraStoreTest extends AbstractIntegrationTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WordCountCassandraStoreTest.class);
+
+    private static final String INPUT_TOPIC = "inputTopic";
+    private static final String OUTPUT_TOPIC = "outputTopic";
+
+    @Test
+    public void shouldCountWords() throws ExecutionException, InterruptedException, TimeoutException {
+        // given
+        final List<String> inputValues = Arrays.asList(
+                "Hello Kafka Streams",
+                "All streams lead to Kafka",
+                "Join Kafka Summit",
+                "И теперь пошли русские слова"
+        );
+        final Map<String, Long> expectedWordCounts = mkMap(
+                mkEntry("hello", 1L),
+                mkEntry("all", 1L),
+                mkEntry("streams", 2L),
+                mkEntry("lead", 1L),
+                mkEntry("to", 1L),
+                mkEntry("join", 1L),
+                mkEntry("kafka", 3L),
+                mkEntry("summit", 1L),
+                mkEntry("и", 1L),
+                mkEntry("теперь", 1L),
+                mkEntry("пошли", 1L),
+                mkEntry("русские", 1L),
+                mkEntry("слова", 1L)
+        );
+
+        // configure and start the processor topology.
+        final Serde<String> stringSerde = Serdes.String();
+        final Serde<Long> longSerde = Serdes.Long();
+        final Properties props = getStreamsProperties();
+
+        // when
+        try (
+                final AdminClient adminClient = initAdminClient();
+                final KafkaProducer<String, String> producer = initProducer(stringSerde, stringSerde);
+                final KafkaConsumer<String, Long> consumer = initConsumer(stringSerde, longSerde);
+                final CqlSession session = initSession();
+                final KafkaStreams streams = initStreams(props, session)
+        ) {
+            // setup input and output topics.
+            int partitions = 6;
+            Collection<NewTopic> topics = Arrays.asList(
+                    new NewTopic(INPUT_TOPIC, partitions, (short) 1),
+                    new NewTopic(OUTPUT_TOPIC, partitions, (short) 1)
+            );
+            adminClient.createTopics(topics).all().get(30, TimeUnit.SECONDS);
+
+            consumer.subscribe(Collections.singletonList(OUTPUT_TOPIC));
+
+            // start streams.
+            streams.start();
+
+            // Step 4: Produce some input data to the input topic.
+            inputValues.forEach(it -> {
+                try {
+                    producer.send(new ProducerRecord<>(INPUT_TOPIC, "testcontainers", it)).get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            // consume and collect streams output
+            final Map<String, Long> results = new HashMap<>();
+            Unreliables.retryUntilTrue(10, TimeUnit.SECONDS, () -> {
+                        ConsumerRecords<String, Long> records = consumer.poll(Duration.ofMillis(100));
+
+                        if (records.isEmpty()) {
+                            return false;
+                        }
+
+                        records.iterator().forEachRemaining(record -> results.put(record.key(), record.value()));
+                        return true;
+                    }
+            );
+
+            // then verify the application's output data.
+            assertThat(results).containsExactlyInAnyOrderEntriesOf(expectedWordCounts);
+        }
+    }
+
+    // note: adapted from https://github.com/confluentinc/kafka-streams-examples/blob/v7.5.0-148/src/test/java/io/confluent/examples/streams/WordCountLambdaIntegrationTest.java
+    @NotNull
+    private KafkaStreams initStreams(Properties streamsConfiguration, CqlSession session) {
+        final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, String> source = builder.stream(INPUT_TOPIC);
+        final Serde<String> stringSerde = Serdes.String();
+        final Serde<Long> longSerde = Serdes.Long();
+
+        final KTable<String, Long> counts = source
+                .peek((k, v) -> LOG.debug("in => {}::{}", k, v))
+                .flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split(" ")))
+                .groupBy((key, value) -> value)
+                .count(Materialized.<String, Long>as(
+                                CassandraStores.builder(session, "word-grouped-count")
+                                        .withKeyspace(CASSANDRA_KEYSPACE)
+                                        .keyValueStore()
+                        )
+                        .withLoggingDisabled()
+                        .withCachingDisabled()
+                        .withKeySerde(stringSerde)
+                        .withValueSerde(longSerde));
+
+        // need to override value serde to Long type
+        counts.toStream()
+                .peek((k, v) -> LOG.debug("out => {}::{}", k, v))
+                .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
+
+        return new KafkaStreams(builder.build(), streamsConfiguration);
+    }
+
+}
